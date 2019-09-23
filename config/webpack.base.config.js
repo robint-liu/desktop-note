1486d10b5b6300dfba27824cddce36779e3e6110
比诺 <bi_nuo@hunliji.com>
2019-09-23 10:06:39 +0800

    webpack: pre-module

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,5 +1,5 @@
 /*
- *  content：entry, output, loader, resolve, some plugins
+ *  content：entry, output, module, resolve, some plugins
  *  考虑点：
  *    一、入口
  *     1、使用路由时，进行单入口打包；
@@ -39,7 +39,7 @@ module.exports = {
       "@lib": path.join(__dirname, "../lib")
     }
   },
-  // 外部扩展
+  // 外部扩展，需要时逐一使用
   externals: {
     // react: "React",
     // "react-dom": "ReactDOM",
@@ -49,5 +49,46 @@ module.exports = {
     // ),
     // antd: "Antd",
     // bizCharts: "BizCharts"
+  },
+  // 模块处理
+  module: {
+    noParse: [], //防止webpack解析那些任何与给定正则表达式相匹配的文件
+    rules:[
+      {
+        test: /\.vue$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js[x]?$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: '/node_modules/',
+        loader: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(sass|scss)$/,
+        exclude: '/node_modules/',
+        loader: ['style-loader', 'css-loader', 'postcss-loader', 'fast-sass-loader'],
+      },
+      {
+        test: /\.less$/,
+        exclude: '/node_modules/',
+        loader: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|git)$/,
+        exclude: '/node_modules/',
+        loader: 'url-loader?limit=8192'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff)/,
+        exclude: '/node_modules/',
+        loader: "file-loader?name=[path][name].[ext]"
+      }
+    ]
   }
 };
